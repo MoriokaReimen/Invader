d62b61df0d5cb2dad4133c7254ed064bf3e5ef08
Kei@Glou-Glout <menschenjager.mark.neun@gmail.com>
2018-08-04 11:20:43 +0900

    [update] Playerクラスにコメントを追加

--- Player.cpp
+++ Player.cpp
@@ -14,17 +14,24 @@
 
 #include<memory>
 
+/*!
+ * @brief Playerクラスのコンストラクタ
+ * @param[in] x 位置のX座標
+ * @param[in] y 位置のY座標
+ */
 Player::Player(const int& x, const int& y) : GameObject(x, y, PLAYER)
 {
 }
 
+/*!
+ * @brief Player状態の更新
+ */
 void Player::update()
 {
+    /*!キー入力を反映*/
     InputKeep* input = GameSystem::getInputKeep();
-    Field* field = GameSystem::getField();
     const KEY_INPUT key = input->get();
     int next_x(x_), next_y(y_);
-
     switch(key) {
     case UP:
         next_y--;
@@ -45,6 +52,8 @@ void Player::update()
         break;
     }
 
+    /*!移動範囲をフィールド上に制限*/
+    Field* field = GameSystem::getField();
     if(field->is_on_field(next_x, next_y)) {
         x_ = next_x;
         y_ = next_y;
@@ -53,15 +62,27 @@ void Player::update()
     return;
 }
 
+/*!
+ * @brief Playerクラスのデストラクタ
+ */
 Player::~Player()
 {}
 
+/*!
+ * @brief Player描画する
+ * @param[in,out] screen 描画するスクリーンのクラス
+ */
 void Player::draw(Screen& screen)
 {
     screen.print("@", this->x_, this->y_, PLAYER_COLOR);
     return;
 }
 
+/*!
+ * @brief 弾を発射する
+ * @param[in] x 弾の生成位置
+ * @param[in] y 弾の生成位置
+ */
 void Player::shoot(const  int&x, const int& y)
 {
     Field* field = GameSystem::getField();
@@ -70,9 +91,14 @@ void Player::shoot(const  int&x, const int& y)
     return;
 }
 
+/*!
+ * @brief 死亡フラグを設定する
+ */
 void Player::kill()
 {
     GameObject::kill();
+
+    /*! ゲームオーバフラグをGameSystemに設定する*/
     GameStatus* status = GameSystem::getStatus();
     status->setGameOver();
 
