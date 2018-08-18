df69c210feb3f4e2bbe8949d355f17ed6e61ef78
Kei@Glou-Glout <menschenjager.mark.neun@gmail.com>
2018-08-18 11:29:37 +0900

    [update] GameTimeクラスにコメントを追加

--- GameTime.cpp
+++ GameTime.cpp
@@ -1,7 +1,18 @@
+/*!
+ * \file GameTime.cpp
+ *
+ * \author MoriokaReimen
+ * \date 2018.08.04
+ * \brief GameTimeクラスの実装
+ */
 #include"GameTime.hpp"
 #include<chrono>
 #include<thread>
 
+/*!
+ * @brief GameTimeクラスのコンストラクタ
+ * @param[in] target_tpf 目標TPF
+ */
 GameTime::GameTime(const int& target_tpf)
     : target_tpf_(target_tpf)
 {
@@ -11,11 +22,17 @@ GameTime::GameTime(const int& target_tpf)
     return;
 }
 
+/*!
+ * @brief GameTimeクラスのデストラクタ
+ */
 GameTime::~GameTime()
 {
     return;
 }
 
+/*!
+ * @brief ゲーム時間を更新する
+ */
 void GameTime::update()
 {
     start_time_ = std::chrono::system_clock::now();
@@ -24,21 +41,29 @@ void GameTime::update()
     return;
 }
 
+/*!
+ * @brief TPFを取得する
+ * @return 現在のTPF
+ */
 int GameTime::getTPF()
 {
     return tpf_;
 }
 
+/*!
+ * @brief 全てのループ終了時に実行されるルーティン
+ */
 void GameTime::onLoopEnd()
 {
+    /*! target_tpf_になるようsleepする */
     auto end_time = std::chrono::system_clock::now();
-
     auto sleep_time = game_time_ - end_time;
     if(sleep_time.count() > 0)
     {
         std::this_thread::sleep_for(sleep_time);
     }
 
+    /*! tpfを更新 */
     auto tpf = std::chrono::system_clock::now() - start_time_;
     tpf_ = std::chrono::duration_cast<std::chrono::milliseconds>(tpf).count();
 
--- GameTime.hpp
+++ GameTime.hpp
@@ -1,11 +1,24 @@
+/*!
+ * \file GameTime.hpp
+ *
+ * \author MoriokaReimen
+ * \date 2018.08.04
+ * \brief GameTimeクラスの宣言
+ */
 #pragma once
 #include<chrono>
 
+/*!
+ * \class GameTime
+ * \brief ゲーム時間管理クラス
+ */
 class GameTime
 {
-    const int target_tpf_;
-    int tpf_;
+    const int target_tpf_; //!< 目標TPF
+    int tpf_; //!< 現在のTPF
+    /*! ループ開始時刻 */
     std::chrono::system_clock::time_point start_time_;
+    /*! ループ終了時刻 */
     std::chrono::system_clock::time_point game_time_;
 
     GameTime(const GameTime& other) = delete;
